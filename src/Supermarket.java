7a431310c7d2221e64fb942d23a80f7faecdbde3
Aleksandr <Aleksandr.zaichikov@simbirsoft.com>
2023-04-21 17:53:45 +0400

    Added workers name for output response of chief

--- src/Supermarket.java
+++ src/Supermarket.java
@@ -7,18 +7,18 @@ public class Supermarket {
         bank.setMoney(70000);
 
         Workers[] worker = new Workers[3];
-        worker[0] = new Workers("Иван", 10000);
-        worker[1] = new Workers("Петр", 25000);
-        worker[2] = new Workers("Анна", 35000);
+        worker[0] = new Workers("Bob", 10000);
+        worker[1] = new Workers("Mike", 25000);
+        worker[2] = new Workers("Jenifer", 35000);
 
         //Будет ли прибавка для каждого из работников +
         for (int i = 0; i < 3; i++) {
             if (chief.getMood() <= 3) {
-                System.out.println("\u001B[34m" + "Chief say: Not today, please");
+                System.out.println("\u001B[34m" + "Chief say: Not today, please, " + worker[i].getName());
             } else if (chief.getMood() > 3 & worker[i].getSalary() < 30000) {
-                System.out.println("\u001B[32m" + "Chief say: Let's do this! Your new salary: " + (worker[i].getSalary() + 3000) + " " + worker[i].getName());
+                System.out.println("\u001B[32m" + "Chief say: Let's do this! Your new salary: " + (worker[i].getSalary() + 3000) + ", " + worker[i].getName());
             } else {
-                System.out.println("\u001B[31m" + "Chief say: Sorry, i don't have more money.");
+                System.out.println("\u001B[31m" + "Chief say: Sorry, i don't have more money, " + worker[i].getName());
             }
             System.out.println("\u001B[33m" + "Worker say: Okay, " + chief.getName());
             System.out.println();
